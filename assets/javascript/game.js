1c058b0b6c993d6c16dac10a3e828807f0d57789
Waleed Hamza <waleedhamza@Waleeds-MacBook-Pro.local>
2018-03-19 11:53:27 -0400

    opponent selection option enabled

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+// game objects each contains a character
+var kenshi = {
+    name: "Kenshi",
+    health: 100,
+    attack: 10,
+    imageUrl: "assets/images/kenshi.png",
+    enemyAttackBack: 15
+};
+
+var subzero = {
+    name: "Sub Zero",
+    health: 100,
+    attack: 10,
+    imageUrl: "assets/images/kenshi.png",
+    enemyAttackBack: 15
+};
+
+var scorpion = {
+    name: "Scorpion",
+    health: 100,
+    attack: 10,
+    imageUrl: "assets/images/kenshi.png",
+    enemyAttackBack: 15
+};
+
+var jade = {
+    name: "Jade",
+    health: 100,
+    attack: 10,
+    imageUrl: "assets/images/kenshi.png",
+    enemyAttackBack: 15
+};
+
+//create object array for all the charactors//
+var character = [kenshi, subzero, scorpion, jade];
+var r = Math.floor(Math.random() * character.length);
+
+//moving the fighter selected to the my fighter section function test//
+var fighterSelected = false
+$("#selectFighter").on("click", ".card", function (event) {
+    if (fighterSelected === false) {
+        fighterSelected = true;
+        $("#myFighter").append($(this))
+    } else if (fighterSelected === true) {
+        $("#opponent").append($(this))
+    }
+});
+
+// add a function to stop inserting fighters in the opponent div when reach is 3
+// fighters// add charectors attack and helth etc. values//
